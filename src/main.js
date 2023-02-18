01733e25f9216a43dc604f84f02a17f8ddca2065
bentonluu <luubenton@gmail.com>
2023-02-18 14:20:24 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+
+import App from "./App.vue";
+import router from "./router";
+
+import "@/assets/styles.scss";
+
+const app = createApp(App);
+
+app.use(PrimeVue, { ripple: true });
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script></script>
+
+<template>
+  <h1>Test App</h1>
+</template>
