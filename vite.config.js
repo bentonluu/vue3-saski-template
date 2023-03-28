a6f7e528e121da7f56438f972e7750a5fb5aa826
bentonluu <luubenton@gmail.com>
2023-03-28 15:31:37 -0600

    Updated vite config to allow env variables

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    // "process.env": process.env,
+    // // By default, Vite doesn't include shims for NodeJS/
+    // // necessary for segment analytics lib to work
+    global: {},
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
+});
